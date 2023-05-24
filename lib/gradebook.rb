82a1dca720e6bea38eaf39f5e22188e6d786227a
Davis Weimer <davisweimer@gmail.com>
2023-05-24 11:00:39 -0600

    lib: Almost finish list_all_students method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -5,9 +5,22 @@ class Gradebook
   def initialize(instructor)
     @instructor = instructor
     @courses = []
+    @students_enrolled = []
+    @gradebook_students = {}
   end
 
   def add_course(course)
     @courses.push(course)
   end
+
+  def list_all_students
+    @gradebook_students
+  end
+
+  def add_students_to_gradebook
+    @gradebook_students[:courses] = @course_1
+  end
 end
+
+
+# {@course_1: @course.students}
