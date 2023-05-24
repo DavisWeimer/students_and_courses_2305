a3e9ed8576506f4743485f58055e1b516e8cf9ef
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:39:08 -0600

    spec: Create passing test for instantiated Course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,6 +2,16 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
-RSpec.describe Course do 
-
+RSpec.describe Course do
+  before(:each) do
+    @course = course = Course.new("Calculus", 2)
+  end
+  describe "classes" do
+    it 'exists' do
+      expect(@course).to be_a(Course)
+      expect(@course.name).to eq("Calculus")
+      expect(@course.capacity).to eq(2)
+      expect(@course.students).to eq([])
+    end
+  end
 end
