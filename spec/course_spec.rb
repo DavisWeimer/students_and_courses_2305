c6fb7e181840f8906ee4db7332f165d3f06235dd
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:54:24 -0600

    spec: Create passing tests for full? & enroll methods

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,6 +5,8 @@ require './lib/student'
 RSpec.describe Course do
   before(:each) do
     @course = course = Course.new("Calculus", 2)
+    @student_1 = Student.new({name: "Morgan", age: 21})
+    @student_2 = Student.new({name: "Jordan", age: 29})
   end
   describe "classes" do
     it 'exists' do
@@ -12,6 +14,20 @@ RSpec.describe Course do
       expect(@course.name).to eq("Calculus")
       expect(@course.capacity).to eq(2)
       expect(@course.students).to eq([])
+    end
+
+    it 'open for enrollment!' do
+      expect(@course.full?).to eq false
+    end
+
+    it 'enrollment is closed, Calculus is full. Nice, I can do something else now!' do
+      expect(@course.full?).to eq false
+
+      @course.enroll(@student_1)
+      @course.enroll(@student_2)
+
+      expect(@course.students).to eq([@student_1, @student_2])
+      expect(@course.full?).to eq true
     end
   end
 end
