b9d663c9c0b3466e71b36dee3834b6c2be7efde4
Davis Weimer <davisweimer@gmail.com>
2023-05-24 11:01:09 -0600

    spec: Create working test for list_all_students method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -6,8 +6,8 @@ require './lib/gradebook'
 
 RSpec.describe Gradebook do
   before(:each) do
-    @course_1 = course = Course.new("Calculus", 2)
-    @course_2 = course = Course.new("Trigonometry", 4)
+    @course_1 = Course.new("Calculus", 2)
+    @course_2 = Course.new("Trigonometry", 4)
     @instructor = Instructor.new({name: "Sir Mathwhizz", age: 104})
     @gradebook = Gradebook.new(@instructor.name)
     @student_1 = Student.new({name: "Morgan", age: 21})
@@ -29,6 +29,22 @@ RSpec.describe Gradebook do
       @gradebook.add_course(@course_2)
 
       expect(@gradebook.courses).to eq([@course_1, @course_2])
+    end
+  end
+
+  describe "students" do
+    it 'list contains all enrolled students' do
+      expect(@gradebook.courses).to eq([])
+      expect(@gradebook.list_all_students).to be_a(Hash)
+      
+      @gradebook.add_course(@course_1)
+      @gradebook.add_course(@course_2)
+      @course_1.enroll(@student_1)
+      @course_1.enroll(@student_2)
+      @gradebook.add_students_to_gradebook
+
+      expect(@gradebook.courses).to eq([@course_1, @course_2])
+      expect(@gradebook.list_all_students).to eq({:@course_1 => @course_1.students})
     end
   end
 end
