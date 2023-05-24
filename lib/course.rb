5c54a6cd7ff903c191f87ededefbe773ee04e634
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:54:01 -0600

    lib: Create full? & enroll methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,11 +1,21 @@
 class Course
   attr_reader :name,
               :capacity,
-              :students
+              :students,
+              :course_cap
   
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @students = []
+    @course_cap = false
+  end
+
+  def full?
+    @students.count == 2
+  end
+
+  def enroll(student)
+    @students.push(student)
   end
 end
