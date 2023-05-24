863e2311d2acd14381d083f0ae3528f326f6c21c
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:32:53 -0600

    lib: Create log_score & grade methods

--- lib/student.rb
+++ lib/student.rb
@@ -7,5 +7,19 @@ class Student
     @name = stats[:name]
     @age = stats[:age]
     @scores = []
+    @score_log = 0
+  end
+
+  def log_score(score)
+    @scores.push(score)
+    @score_log += 1
+  end
+
+  def grade
+    sum = 0
+    @scores.each do |score|
+      sum += score
+    end
+    sum.to_f / @score_log
   end
 end
