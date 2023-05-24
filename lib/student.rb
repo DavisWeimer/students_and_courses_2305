2f273c8a7788cb25be22c9cf8c812b18f4eab2ac
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:16:08 -0600

    lib: Create instanced attributes for Student

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
 class Student
- 
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(stats)
+    @name = stats[:name]
+    @age = stats[:age]
+    @scores = []
+  end
 end
