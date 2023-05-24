dd91f3e6f0c390d91ec8bf7035a059f188c34213
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:33:25 -0600

    spec: Create passing tests for log_score & grade methods

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -12,9 +12,26 @@ RSpec.describe Student do
       expect(@student.age).to eq(21)
     end
 
-    it 'have and empty scores array' do
+    it 'has an empty scores array' do
       expect(@student.scores).to be_a(Array)
       expect(@student.scores).to eq([])
+    end
+
+    it 'can log a score' do
+      expect(@student.scores).to eq([])
+
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+    end
+
+    it 'can find the average of all logged scores' do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+      expect(@student.grade).to eq(83.5)
     end
   end
 end
