71479762bdfe2bc70c233117b38f6d90be246f14
Davis Weimer <davisweimer@gmail.com>
2023-05-24 09:17:00 -0600

    spec: Create passing test for instantiated Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,19 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+  describe "stats" do
+    it 'exists' do
+      expect(@student).to be_a(Student)
+      expect(@student.name).to eq("Morgan")
+      expect(@student.age).to eq(21)
+    end
+
+    it 'have and empty scores array' do
+      expect(@student.scores).to be_a(Array)
+      expect(@student.scores).to eq([])
+    end
+  end
 end
